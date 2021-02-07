2fdf74e703a5f38151ef54d92c8a9ee6f48bb24a
seagleax <seagleax700@gmail.com>
2021-02-08 02:48:42 +0700

    change url spreadsheet

--- public/form.js
+++ public/form.js
@@ -1,6 +1,21 @@
 async function contact() {
+    if ($('input[name="full_name"]').val() === ""){
+        alert('Nama Lengkap Tidak Boleh Kosong!')
+        return false
+    }
+
+    if ($('input[name="phone_number"]').val() === "") {
+        alert('Nomer Telepon Tidak Boleh Kosong!')
+        return false
+    }
+
+    if ($('select[name="province"]').val() === "") {
+        alert('Provinsi Harus Dipilih!!')
+        return false
+    }
+
     $.ajax({
-        url:'https://api.apispreadsheets.com/data/7640/',
+        url:'https://api.apispreadsheets.com/data/7848/',
         type:'post',
         data:$('form[name="form-contact"]').serializeArray(),
         success: function(){
